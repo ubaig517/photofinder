5535b86885cc3cbce5093dfd828f99959bf58c08
Umair <ubaig517@gmail.com>
2018-09-20 14:10:42 -0400

    Changed image API to Unsplash API allowing for search with image ID. Built functionality enabling user to click image to view full page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,8 @@
-const API_KEY = "10150327-3e63ff662691ad227a4537a47";
-const URL = `https://pixabay.com/api/?key=${API_KEY}&per_page=100&q=`;
+// const API_KEY = "10150327-3e63ff662691ad227a4537a47";
+// const URL = `https://pixabay.com/api/?key=${API_KEY}&per_page=100&q=`;
+
+const API_KEY = 'c85be81a5d91495b84d935e25305df9414c4a025e7930ae75e644ae7fa77d68d';
+const URL = `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&per_page=20&query=`;
 
 $(document).ready(() => {
   $('#search-form').on('submit', (e) => {
@@ -14,13 +17,13 @@ $(document).ready(() => {
 const findImage = (searchTerm) => {
   axios.get(`${URL + searchTerm}`)
     .then((res) => {
-      let imageData = res.data.hits;
+      let imagesData = res.data.results;
       let output = '';
-      $.each(imageData, (index, image) => {
+      $.each(imagesData, (index, image) => {
         output += `
           <div class="col-md-3">
             <div class="card-body text-center">
-              <img src="${image.largeImageURL}">
+              <a onclick="selectedImage('${image.id}')"><img src="${image.urls.small}"></a>
             </div>
           </div>
         `;
@@ -32,3 +35,39 @@ const findImage = (searchTerm) => {
       console.log(error);
     });
 }
+
+function selectedImage(id) {
+  sessionStorage.setItem('imageId', id);
+  window.location = 'image-page.html';
+  return false;
+}
+
+function getStoredImage() {
+  let storedImageId = sessionStorage.getItem('imageId');
+
+  let storedImageUrl = `https://api.unsplash.com/photos/${storedImageId}/?client_id=${API_KEY}`;
+
+  axios.get(`${storedImageUrl}`)
+    .then((res) => {
+      console.log(res);
+      let image = res.data.urls;
+      let output = `
+          <div>
+            <img src="${image.regular}" />
+          </div>
+        `;
+
+      $('#image-container').html(output);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
+
+// Unsplash aki key = c85be81a5d91495b84d935e25305df9414c4a025e7930ae75e644ae7fa77d68d
+// Unsplash query syntax
+// https://api.unsplash.com/search/photos/?client_id=c85be81a5d91495b84d935e25305df9414c4a025e7930ae75e644ae7fa77d68d&query=party
+
+// search with id = "https://api.unsplash.com/photos/DX56fYvJeis/?client_id=c85be81a5d91495b84d935e25305df9414c4a025e7930ae75e644ae7fa77d68d"
