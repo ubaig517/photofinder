ed652b731a19bb92d1ac12ec08defde14fb9f077
Umair <ubaig517@gmail.com>
2018-09-24 16:20:40 -0400

    removal of jQuery dependancies

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,23 @@
-// const API_KEY = "10150327-3e63ff662691ad227a4537a47";
-// const URL = `https://pixabay.com/api/?key=${API_KEY}&per_page=100&q=`;
-
 const API_KEY = 'c85be81a5d91495b84d935e25305df9414c4a025e7930ae75e644ae7fa77d68d';
 const URL = `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&per_page=20&query=`;
 
-$(document).ready(() => {
-  $('#search-form').on('submit', (e) => {
-    let searchTerm = $('#search-term').val();
+document.querySelector('#search-form').addEventListener('submit', (e) => {
+  let searchTerm = document.querySelector('#search-term').value;
 
-    findImage(searchTerm);
-
-    e.preventDefault();
-  });
+  findImage(searchTerm);
+  console.log(searchTerm);
+  e.preventDefault();
 });
 
 const findImage = (searchTerm) => {
   axios.get(`${URL + searchTerm}`)
     .then((res) => {
+
       let imagesData = res.data.results;
+
       let output = '';
-      $.each(imagesData, (index, image) => {
+
+      imagesData.forEach((image) => {
         output += `
           <div class="col-md-3">
             <div class="card-body text-center">
@@ -29,7 +27,7 @@ const findImage = (searchTerm) => {
         `;
       });
 
-      $('#images').html(output);
+      document.querySelector('#images').innerHTML = output;
     })
     .catch((error) => {
       console.log(error);
@@ -57,7 +55,7 @@ function getStoredImage() {
           </div>
         `;
 
-      $('#image-container').html(output);
+      document.querySelector('#image-container').innerHTML = output;
     })
     .catch((error) => {
       console.log(error);
